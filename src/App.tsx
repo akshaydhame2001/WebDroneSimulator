3a5d690cc356546f7f3ee71f98962111b033bccf
Harshal <akshaydhame2001@gmail.com>
2024-05-24 13:08:12 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState } from "react"
 import Map, { Marker, Source, Layer } from "react-map-gl"
 import Sidebar from "./Sidebar"
 import DroneMarker from "./DroneMarker"
